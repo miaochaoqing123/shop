6aa24d284d37abd6fbcb9d7352b194e8533c5abc
缪超青 <419666193@qq.com>
2018-05-02 13:21:49 +0800

    'goods的素材添加'

--- apps/goods/models.py
+++ apps/goods/models.py
@@ -76,7 +76,7 @@ class GoodsImage(models.Model):
     """商品轮播图"""
     goods = models.ForeignKey(Goods, verbose_name='商品', related_name='images')
     image = models.ImageField(upload_to='', verbose_name='图片', null=True, blank=True)
-    image_url = models.CharField(max_length=300, null=True, blank=True, verbose_name='图片url')
+    # image_url = models.CharField(max_length=300, null=True, blank=True, verbose_name='图片url')
     add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
 
     class Meta:
